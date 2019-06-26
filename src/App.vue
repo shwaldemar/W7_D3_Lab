5be1225c5ea1d93620b71b287d851caf363e7c01
Kyle Walker <Randomer44@hotmail.com>
2019-06-26 15:43:46 +0100

    Gotten the list of countries from the API and displayed it on the screen unformated.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<p> {{this.countryList}}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      countryList: []
+    }
+  },
+  mounted() {
+    fetch('https://restcountries.eu/rest/v2/all')
+            .then(responce => responce.json())
+            .then(countries => this.countryList = countries)
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
